6b0e2b8635beaffcf1f81d7f69e5a0a0dc2cd223
Chander Rajput <36323250+chanderrajput@users.noreply.github.com>
2020-06-12 18:59:17 +0500

    Update ChaseUpTest.java

--- ChaseUpTest.java
+++ ChaseUpTest.java
@@ -2,172 +2,173 @@ import java.util.Date;
 import java.util.*;
 public class ChaseUpTest
 {
+	private static Scanner scan;
 	public static void main(String[] args)
 	{
-        	Scanner scan=new Scanner(System.in);
+        scan = new Scanner(System.in);
 		int id=0;
 
 		Date date=new Date();
-        	ChaseUp ch1=new ChaseUp("The New ChaseUp Parking Shoping centre","Karachi",03403215000,"chaseup@gmail.com",100,0,date);
-        	ChaseUpFood f1=new ChaseUpFood();
-        	ChaseUpCosmetics c1=new ChaseUpCosmetics();
-        	ChaseUpReception r1= new ChaseUpReception();
-		ChaseUpPayment p1= new ChaseUpPayment();
-		ChaseUpcustomer csmr =new ChaseUpcustomer();
-		ChaseUpVehicleType parkType = new ChaseUpVehicleType();
+		ChaseUp chaseUp=new ChaseUp("The New ChaseUp Parking Shoping centre","Karachi",03403215000,"chaseup@gmail.com",100,0,date);
+		ChaseUpFood foodArea=new ChaseUpFood();
+		ChaseUpCosmetics cosmeticArea=new ChaseUpCosmetics();
+		ChaseUpReception reception= new ChaseUpReception();
+		ChaseUpPayment payment= new ChaseUpPayment();
+		ChaseUpcustomer customer =new ChaseUpcustomer();
 		ChaseUpVehicalCar vehicalCar = new ChaseUpVehicalCar();
 		ChaseUpVehicalBike vehicalBike = new ChaseUpVehicalBike();		
 
-	        ArrayList<ChaseUpVehicleType> vehical1 = new ArrayList<ChaseUpVehicleType>();
 		ArrayList<ChaseUpReception> customerList = new ArrayList<ChaseUpReception>();
 		ArrayList<ChaseUpVehicalBike> bikeList = new ArrayList<ChaseUpVehicalBike>();
 		ArrayList<ChaseUpVehicalCar> carList = new ArrayList<ChaseUpVehicalCar>();
 
 
-        	System.out.println("\n\n                    ====>      Wellcome to the New ChaseUp Parking Shoping centre      <====");
+        System.out.println("\n\n                    ====>      Wellcome to the New ChaseUp Parking Shoping centre      <====");
 
 		System.out.println("\n\nEnter customer's name:");
-		csmr.setName(scan.nextLine());
+		customer.setName(scan.nextLine());
 
 		System.out.println("Enter customer's address:");
-		csmr.setAddress(scan.nextLine());
+		customer.setAddress(scan.nextLine());
 
 		System.out.println("Enter customer's emailaddress:");                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                     
-		csmr.setEmail(scan.nextLine());
+		customer.setEmail(scan.nextLine());
 
 		System.out.println("enter customer's cellNo:");
-		csmr.setCellNo(scan.nextLong());
+		customer.setCellNo(scan.nextLong());
 
 
-		System.out.println("\n\n             Hello "+csmr.getName());
+		System.out.println("\n\n             Hello "+customer.getName());
 
-		            	System.out.println("\n\nSelect the vehical Type: \n");
+		System.out.println("\n\nSelect the vehical Type: \n");
 
-		            	System.out.println("1. Car.\n2. Bike.");
-		            	int type = scan.nextInt();
+		System.out.println("1. Car.\n2. Bike.");
+		
+		boolean conditoin=true;
+		int type=0;
 
-				if(type!=1 && type!=2)
+		while(conditoin){
+			type = scan.nextInt();
+			if(type==1 && type==2){
+				conditoin=false;
+			}else{
+				System.out.println("Please select the correct input.\n");
+			}
+		}
+
+		
+
+		switch (type)
+		{
+			case 1:
 				{
-					System.out.println("Please select the correct input.");
-		            		System.out.println("\n\nSelect the vehical Type: \n");
-		            		System.out.println("1. Car.\n2. Bike.");
-		            		type = scan.nextInt();			
-				}
+					vehicalCar.setType("car");
+					System.out.println("Enter the car plate number:");
+					vehicalCar.setNoPlate(scan.next());
+					vehicalCar.setBookigId(id+=1);
+					chaseUp.vehicalIn();
+					customerList.add(reception);
+					carList.add(vehicalCar);
+					customer.setCstmrCar(carList);
+					System.out.println("\n"+customer.car());
+					System.out.println("\nPlease go to parking no "+vehicalCar.getBookigId());
+					break;
+		}
+		case 2:
+			{
+			vehicalBike.setType("bike");
+				System.out.println("Enter the bike plate number:");
+				vehicalBike.setNoPlate(scan.next());						                    	
+				vehicalBike.setBookigId(id+=1);
+				chaseUp.vehicalIn();
+				customerList.add(reception);
+				bikeList.add(vehicalBike);
+				customer.setCstmrBike(bikeList);
+				System.out.println("\n"+customer.bike());
+				System.out.println("\nPlease go to parking no "+vehicalBike.getBookigId()+" for Shoping.");
 
-            			switch (type)
-            			{
-                			case 1:
-                    			{
-		    				vehicalCar.setType("car");
-                    				System.out.println("Enter the car plate number:");
-                    				vehicalCar.setNoPlate(scan.next());
-						vehicalCar.setBookigId(id+=1);
-						ch1.vehicalIn();
-        			            	customerList.add(r1);
-					    	carList.add(vehicalCar);
-						csmr.setCstmrCar(carList);
-						System.out.println("\n"+csmr.car());
-						System.out.println("\nPlease go to parking no "+vehicalCar.getBookigId());
-
-        			            	break;
-        			  	}
-                			case 2:
-                    			{
-		    				vehicalBike.setType("bike");
-                    				System.out.println("Enter the bike plate number:");
-                    				vehicalBike.setNoPlate(scan.next());						                    	
-		    				vehicalBike.setBookigId(id+=1);
-						ch1.vehicalIn();
-						customerList.add(r1);
-		    				bikeList.add(vehicalBike);
-						csmr.setCstmrBike(bikeList);
-
-						System.out.println("\n"+csmr.bike());
-						System.out.println("\nPlease go to parking no "+vehicalBike.getBookigId()+" for Shoping.");
-
-                    				break;
-                    			}
-                			default:
-                    			{
-                    				System.out.println("Please enter the write input");
-						System.exit(0);
-                    			}
-			
-            			}
+				break;
+			}
+		default:
+			{
+				System.out.println("Please enter the write input");
+				System.exit(0);
+			}
+		}
 
 		System.out.println("\n\nSelect the area Type \n");
 
-        	System.out.println("1. Food Area\n2. Cosmetics Area");
-        	int area=scan.nextInt();
+		System.out.println("1. Food Area\n2. Cosmetics Area");
+		int area=0;
+		while(!conditoin){
+			area=scan.nextInt();
+			if(area==1 && area==2){
+				conditoin=true;
+			}else{
+				System.out.println("Please select the correct input.\n");
+			}
 
-		if(area!=1 && area!=2)
-		{
-			System.out.println("Please select the correct input.");
-			System.out.println("\n\nSelect the area Type \n");
-	        	System.out.println("1. Food Area\n2. Cosmetics Area");
-	        	area=scan.nextInt();
 		}
-
-	
-        	if(area==1)
+		
+		if(area==1)
 		{
 			System.out.println("\n\n       ===>   Welcome to the Food Market   <===\n");
 
-        		f1.order();
-			f1.discount(f1.getTotalPrice());
+			foodArea.order();
+			foodArea.discount(foodArea.getTotalPrice());
  	
 	
-        		System.out.println("\nYour total bill of Food item is "+f1.getTotalPrice());
-			System.out.println("\nOn the Food shoping of "+f1.getTotalPrice()+" you get the discount of "+f1.getDiscount());
+			System.out.println("\nYour total bill of Food item is "+foodArea.getTotalPrice());
+			System.out.println("\nOn the Food shoping of "+foodArea.getTotalPrice()+" you get the discount of "+foodArea.getDiscount());
 
-			f1.setTotalPrice((f1.getTotalPrice()-f1.getDiscount()));
-        		System.out.println("\nYour total bill after discount is "+f1.getTotalPrice());
+			foodArea.setTotalPrice((foodArea.getTotalPrice()-foodArea.getDiscount()));
+			System.out.println("\nYour total bill after discount is "+foodArea.getTotalPrice());
 	
 			System.out.println("\n\ndo you want to go to cosmetic area.\nIf yes then press Y else N: ");
 			String nextArea=scan.next();
 	
-			if(nextArea.equals("y") || nextArea.equals("Y"))
+			if(nextArea.equalsIgnoreCase("y"))
 			{
 				System.out.println("\n\n       ===>   Welcome to the Cosmetic Market   <===\n");
 	
-		        	c1.order();
-	    			c1.discount(c1.getTotalPrice());
+				cosmeticArea.order();
+				cosmeticArea.discount(cosmeticArea.getTotalPrice());
 
-			        System.out.println("\nYour total bill of cosmetics item is "+c1.getTotalPrice());
-			 	System.out.println("\nOn the cosmetics shoping of "+c1.getTotalPrice()+" you get the discount of "+c1.getDiscount());
+				System.out.println("\nYour total bill of cosmetics item is "+cosmeticArea.getTotalPrice());
+			 	System.out.println("\nOn the cosmetics shoping of "+cosmeticArea.getTotalPrice()+" you get the discount of "+cosmeticArea.getDiscount());
 
-				c1.setTotalPrice((c1.getTotalPrice()-c1.getDiscount()));
-			        System.out.println("\nYour total bill of cosmetics item after discount is "+c1.getTotalPrice());
+				cosmeticArea.setTotalPrice((cosmeticArea.getTotalPrice()-cosmeticArea.getDiscount()));
+			    System.out.println("\nYour total bill of cosmetics item after discount is "+cosmeticArea.getTotalPrice());
 			}			
 		}
 		else if(area==2)
 		{
 			System.out.println("\n\n       ===>   Welcome to the Cosmetic Market   <===\n");
 
-		        c1.order();
-			c1.discount(c1.getTotalPrice());
+		    cosmeticArea.order();
+			cosmeticArea.discount(cosmeticArea.getTotalPrice());
 	
-		        System.out.println("\nYour total bill of Cosmetics item is "+c1.getTotalPrice());
- 			System.out.println("\nOn the cosmetic shoping of "+c1.getTotalPrice()+" you get the discount of "+c1.getDiscount());
+		    System.out.println("\nYour total bill of Cosmetics item is "+cosmeticArea.getTotalPrice());
+ 			System.out.println("\nOn the cosmetic shoping of "+cosmeticArea.getTotalPrice()+" you get the discount of "+cosmeticArea.getDiscount());
 
-			c1.setTotalPrice((c1.getTotalPrice()-c1.getDiscount()));
- 		        System.out.println("\nYour total bill after discount is "+c1.getTotalPrice());
+			cosmeticArea.setTotalPrice((cosmeticArea.getTotalPrice()-cosmeticArea.getDiscount()));
+ 		    System.out.println("\nYour total bill after discount is "+cosmeticArea.getTotalPrice());
 
 			System.out.println("\n\ndo you want to go to Food area.\nIf yes then press Y else press N:	");
 			String nextArea=scan.next();
 	
-			if(nextArea.equals("y") || nextArea.equals("Y"))
+			if(nextArea.equalsIgnoreCase("Y"))
 			{
 				System.out.println("\n\n       ===>   Welcome to the Food Market    <===\n");
 
-        			f1.order();
-				f1.discount(f1.getTotalPrice());
+        		foodArea.order();
+				foodArea.discount(foodArea.getTotalPrice());
  	
-				System.out.println("\nYour total bill of Food item is "+f1.getTotalPrice());
-				System.out.println("\nOn the Food shoping of "+f1.getTotalPrice()+" you get the discount of "+f1.getDiscount());
+				System.out.println("\nYour total bill of Food item is "+foodArea.getTotalPrice());
+				System.out.println("\nOn the Food shoping of "+foodArea.getTotalPrice()+" you get the discount of "+foodArea.getDiscount());
 
-				f1.setTotalPrice((f1.getTotalPrice()-f1.getDiscount()));
-			        System.out.println("\nYour total bill after discount is "+f1.getTotalPrice());
+				foodArea.setTotalPrice((foodArea.getTotalPrice()-foodArea.getDiscount()));
+			        System.out.println("\nYour total bill after discount is "+foodArea.getTotalPrice());
 			}
 
 
@@ -178,23 +179,23 @@ public class ChaseUpTest
 			System.exit(0);
 		}
 
-		r1.setFinalBill((f1.getTotalPrice()+c1.getTotalPrice()));
-		p1.setCash(r1.getFinalBill());
+		reception.setFinalBill((foodArea.getTotalPrice()+cosmeticArea.getTotalPrice()));
+		payment.setCash(reception.getFinalBill());
 		
 
-			System.out.println("\n\nyour final bill is "+r1.getFinalBill());
-			p1.paymentOption();			
+		System.out.println("\n\nyour final bill is "+reception.getFinalBill());
+		payment.paymentOption();			
 
-			System.out.println("\n\n	===>  Here is your recept.  <===");
-			System.out.println(ch1.toString());						
-			System.out.println(csmr.toString());
-			System.out.println("Customer bill is "+r1.getFinalBill());
-			r1.terms();
+		System.out.println("\n\n	===>  Here is your recept.  <===");
+		System.out.println(chaseUp.toString());						
+		System.out.println(customer.toString());
+		System.out.println("Customer bill is "+reception.getFinalBill());
+		reception.terms();
 
-			System.out.println("\n\nPlease collect your bags from exit gate by showing this recipt.");
+		System.out.println("\n\nPlease collect your bags from exit gate by showing this recipt.");
 
 
-			System.out.println("\n\n                   ===>   Thanks for choosing ChaseUp Shoping centre   <===");
+		System.out.println("\n\n                   ===>   Thanks for choosing ChaseUp Shoping centre   <===");
 
 	}
 }
